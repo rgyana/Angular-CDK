6e4a43f22670d734b6791da5e6cdbf902d93e306
Gyana <rgyanaranjan58@gmail.com>
2022-12-12 11:06:03 +0530

    committing on 11:05

--- Portals_CDK/src/app/app.component.ts
+++ Portals_CDK/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { ComponentPortal, Portal, TemplatePortal, DomPortal} from '@angular/cdk/Portal'
+import { Comp1Component } from './comp1/comp1.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Portals_CDK';
+export class AppComponent implements AfterViewInit {
+  
+  constructor(private _viewContainerRef: ViewContainerRef) {}
+ 
+  @ViewChild('templatePortalContent') templatePortalContent : TemplateRef<unknown>;
+  @ViewChild('domPortalContent') domPortalContent : ElementRef<HTMLElement>;
+
+  selectedPortal: Portal<any>;
+  componentPortal : ComponentPortal<any>;
+  templatePortal : TemplatePortal<any>;
+  domPortal : DomPortal<any>
+
+  ngAfterViewInit() {
+    this.componentPortal = new ComponentPortal(Comp1Component);
+    this.templatePortal = new TemplatePortal(this.templatePortalContent, this._viewContainerRef);
+    this.domPortal = new DomPortal(this.domPortalContent);
+  }
 }
